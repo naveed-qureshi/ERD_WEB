cd5f35938e19bdb5cdb42c10755f6cea7c4b4596
Muhammad Naveed <makhdoom47012@gmail.com>
2020-12-08 11:25:13 +0500

    Done with razor pages with code behind file

---
+++ Lecture47RazorPagesWithCodeBehindFile/Lecture34IntroToWeb/Pages/Index1.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lecture34IntroToWeb.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public string DayName { get; set; }
+        public string CurrentTime { get; set; }
+
+        public void OnGet ( )
+        {
+            DayName = System.DateTime.Now.ToString ("dddd");
+            CurrentTime = System.DateTime.Now.ToString ();
+        }
+    }
+}
